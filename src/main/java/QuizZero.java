cf6df23533b91ee8797a696b5bfbfe14dccb54da
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-02-28 23:11:56 -0500

    "solved Quiz 0"

--- src/main/java/QuizZero.java
+++ src/main/java/QuizZero.java
@@ -17,41 +17,101 @@ public class QuizZero {
 
     // return zero
     public static  int Qzero(int a, int b) {
-        return -1;
+        return 0;
     }
 
     // return the sum of a and b
     public static  int Qone(int a, int b) {
-        return 0;
+        return a + b ;
     }
 
     //return the product
-    public static  int Qtwo(int a, int b) {  return 0; }
+    public static  int Qtwo(int a, int b) {
+        return a * b ;
+    }
+
     //return the difference of a and b
-    public static  int Qthree(int a, int b) { return 0; }
+    public static  int Qthree(int a, int b) {
+
+        return b - a;
+    }
+
     //return the nagation of a
-    public static  int Qfour(int a) { return 0; }
+    public static  int Qfour(int a) {
+
+        return -a; }
     // write code to pass the test
-    public static  int Qfive(int a, int b) {return 0; }
+
+    public static  int Qfive(int a, int b) {
+
+        return a * 2 ; }
+
     // write code to pass the test
-    public static  int Qsix(int a) { return 0; }
+
+    public static  int Qsix(int a) {
+
+        return a * a * a; }
+
     // write code to pass the test
-    public static  int Qseven(int a, int b) { return 0; }
+    public static  int Qseven(int a, int b) {
+
+        return a % b ; }
+
     //return the sum of a and b divded by c
-    public static  int Qeight(int a, int b, int c) { return 0; }
+    public static  int Qeight(int a, int b, int c) {
+
+        return (a + b) / c; }
+
     // write code to pass the test
-    public static  int Qnine(int a, int b) { return 0; }
+    public static  int Qnine(int a, int b) {
+
+        return a * b + 5; }
     // write code to pass the test
-    public static  boolean Qaay(boolean a, boolean b) { return false; }
+    public static  boolean Qaay(boolean a, boolean b) {
+
+        if (a && b) {
+            return true;
+        }
+        else
+
+        return false; }
     // write code to pass the test
-    public static  boolean Qbee(boolean a, boolean b) { return false; }
+    public static  boolean Qbee(boolean a, boolean b) {
+
+        if (a || b) {
+
+            return true;
+        }
+        else
+
+        return false; }
     // write code to pass the test
-    public static  int Qcee(boolean a, boolean b) { return 0; }
+    public static  int Qcee(boolean a, boolean b) {
+
+        if (a == b) {
+
+            return -2;
+        } else {
+            return 1; }
+    }
     //return x dvided by b squared
-    public static  float Qdee(float x, int b) { return 0.0f; }
+    public static  float Qdee(float x, int b) {
+
+        return x / (b * b); }
+
     // if x-y is less than 1/10,000 return zero otherwise return 1
-    public static  int Qeee(double x, double y) { return 0; }
+    public static  int Qeee(double x, double y) {
+        if ( x-y < .0001 )
+
+        return 0;
+    } else {
+
+        return 1; }
+
+
     //if a is zero return zero, if a is even return a random number times zero, if not even return 5 - 5
-    public static  int Qeff(int a) { return -99999999; }
+    public static  int Qeff(int a) {
+
+        return 0 ; }
 
 }
